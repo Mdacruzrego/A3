babf2d4f49434c2ef58aef8989e78daa2ecc406f
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2022-11-17 13:56:40 -0300

    Create implementação.java

---
+++ implementação.java
@@ -0,0 +1,73 @@
+public class Login{
+public String getEmail() {
+return email;
+}
+public void setEmail(string email) {
+this.login = email;
+}
+public String getSenha() {
+return senha;
+}
+public void setSenha(string senha) {
+this.senha = senha;
+}
+}
+public class Cadastro{
+public String getPrimeiroNome() {
+return primeironome;
+}
+public void setPrimeiroNome(string primeironome) {
+this.primeironome = primeironome;
+}
+public String getSobreome() {
+return sobreonome;
+}
+public void setSobreonome(string sobreonome) {
+this.sobreonome = sobreonome;
+}
+public void setEmail(string email) {
+this.login = email;
+}
+public String getSenha() {
+return senha;
+}
+public void setSenha(string senha) {
+this.senha = senha;
+}
+public String getConfirmarSenha() {
+return confirmarsenha;
+}
+public void setConfirmarSenha(string confirmarsenha) {
+this.confirmarsenha = confirmarsenha;
+}
+}
+public class PontoDeColeta{
+public String getEndereço() {
+return endereço;
+}
+public void setEndereço(string endereço) {
+this.endereço = endereço;
+}
+public String getTipoMaterial() {
+return tipomaterial;
+}
+public void setTipoMaterial(string tipomaterial) {
+this.tipomaterial = tipomaterial;
+}
+public int getHorarioColeta() {
+return horariocoleta;
+}
+public void setHorarioColeta(int horariocoleta) {
+this.horariocoleta = horariocoleta;
+}
+public int getDiasColeta() {
+return diascoleta;
+}
+public void setDiasColeta(int diascoleta) {
+this.diascoleta = diascoleta;
+}
+}
+
+
+
+
